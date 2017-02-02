d7c9836052a680fa0b707daa076e49b34c73dcbd
J0hn8uff3r <bufferoverflow@hotmail.es>
2017-02-02 01:12:44 +0100

    Add files via upload

---
+++ Codeshare_Selector_Fixer.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        Codeshare Selector Fixer
+// @require     https://code.jquery.com/jquery-3.1.1.min.js
+// @namespace   Codeshare Selector Fixer
+// @description Change default highlight selection color to let you view correctly selected text
+// @include     https://codeshare.io/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+$('div').css({"font-size": "18px", "color":"red", "background-color":"black"});
