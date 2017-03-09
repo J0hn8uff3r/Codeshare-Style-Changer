65fd6a5180e5e11d433d91026a43fffaade2b6d8
J0hn8uff3r <bufferoverflow@hotmail.es>
2017-03-09 19:06:38 +0100

    Update Codeshare_Selector_Fixer.user.js

--- Codeshare_Selector_Fixer.user.js
+++ Codeshare_Selector_Fixer.user.js
@@ -9,3 +9,16 @@
 // ==/UserScript==
 
 $(".cm-s-monokai").css({"font-size": "18px", "color":"red", "background-color":"black"});
+
+document.onkeydown = function (e) {
+    e = e || window.event;//Get event
+    if (e.ctrlKey) {
+        var c = e.which || e.keyCode;//Get key code
+        switch (c) {
+            case 83://Disable Ctrl+S
+                e.preventDefault();     
+                e.stopPropagation();
+            break;
+        }
+    }
+};
